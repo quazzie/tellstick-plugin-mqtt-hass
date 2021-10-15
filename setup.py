2d0aa0f413e5bcc2c409b2d5801d172e766659d6
Tommy Jonsson <quazzie@gmail.com>
2021-10-15 15:40:48 +0200

    new release .90.1, cover and climate fixes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='MQTT Homeassistant',
-    version='0.90.0',
+    version='0.90.1',
     description='Plugin to connect to Homeassistant via MQTT Autodiscover',
     icon='hass.png',
     color='#660066',
